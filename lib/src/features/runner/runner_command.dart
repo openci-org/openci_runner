29bb87293dd9cc99e8ae0354ea20a959e9e30322
Masahiro Aoki <freud427@gmail.com>
2024-04-10 01:59:50 +0900

    fix: import order lint

--- lib/src/features/runner/runner_command.dart
+++ lib/src/features/runner/runner_command.dart
@@ -9,18 +9,15 @@ import 'package:dart_firebase_admin/firestore.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 import 'package:mason_logger/mason_logger.dart';
-import 'package:openci_runner/src/services/build_job/flutter_version_manager.dart';
-import 'package:openci_runner/src/services/build_job/ipa_build_service.dart';
-import 'package:openci_runner/src/services/build_job/organization/organization_model.dart';
-import 'package:openci_runner/src/services/build_job/workflow/workflow_service.dart';
-import 'package:process_run/shell.dart';
-import 'package:sentry/sentry.dart';
-
 import 'package:openci_runner/src/features/job/domain/job_data.dart';
 import 'package:openci_runner/src/features/job/domain/job_data_v2.dart';
 import 'package:openci_runner/src/features/vm/controller/vm_controller.dart';
 import 'package:openci_runner/src/services/build_job/build_utility_service.dart';
+import 'package:openci_runner/src/services/build_job/flutter_version_manager.dart';
+import 'package:openci_runner/src/services/build_job/ipa_build_service.dart';
+import 'package:openci_runner/src/services/build_job/organization/organization_model.dart';
 import 'package:openci_runner/src/services/build_job/workflow/workflow_model.dart';
+import 'package:openci_runner/src/services/build_job/workflow/workflow_service.dart';
 import 'package:openci_runner/src/services/log/log_service.dart';
 import 'package:openci_runner/src/services/shell/local_shell_service.dart';
 import 'package:openci_runner/src/services/shell/ssh_shell_service.dart';
@@ -28,6 +25,8 @@ import 'package:openci_runner/src/services/ssh/ssh_service.dart';
 import 'package:openci_runner/src/services/tart/tart_service.dart';
 import 'package:openci_runner/src/services/uuid/uuid_service.dart';
 import 'package:openci_runner/src/services/vm_service.dart';
+import 'package:process_run/shell.dart';
+import 'package:sentry/sentry.dart';
 
 Future<String> getInstallationToken(
   int appId,
